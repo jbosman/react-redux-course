4dfa2fac2b192f418a6011755f126868849e2e2b
Joe Bosman <joseph.a.bosman@gmail.com>
2017-07-21 10:25:32 -0400

    Created controlled search bar componenet and app function for getting videos from the youtube api.

--- browser/react/app/index.js
+++ browser/react/app/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import YTSearch from 'youtube-api-search';
+
+import SearchBar from '../components/search-bar';
+
+const API_KEY = 'AIzaSyDgGYqXUfcvqlXXzR6_V8CfyDZpafDgB1I';
+
+YTSearch({ key: API_KEY, term:'surfboards'}, (data) => console.log(data))
 
 require('./index.scss');
 
 export default function(){
 	return (
 		<div className='app-component'>
-			<h1>Hello, React Template</h1>
+			<SearchBar />
 		</div>
 	)
 }
--- browser/react/components/search-bar/index.js
+++ browser/react/components/search-bar/index.js
@@ -1,3 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 require('./index.scss');
+
+export default class SearchBar extends Component {
+
+	constructor(props){
+		super(props);
+		this.state = { term: 'search' };
+		this.onInputChange = this.onInputChange.bind(this);
+	}
+
+	onInputChange(event){
+		this.setState({ term: event.target.value });
+	}
+
+	render(){
+
+		const { term } = this.state;
+
+		return (
+			<div className='search-bar'>
+				<input 
+					value={ term }
+					onChange={ this.onInputChange } 
+					type="text" placeholder="search" />
+				<div>
+					Value of the input: { term }
+				</div>
+			</div>
+		);
+	}
+}
